4fc6c41fbb068ae3712354d1c857b1eb0ebfb3c0
sarath sekar <6395a29dfbf54baf29030ac9@bots.bitbucket.org>
2025-03-24 00:02:39 +0530

    files updated to change repo

--- commons/gamelaunch/src/main/java/org/cashempire/gamelaunch/util/Static.java
+++
@@ -1,8 +0,0 @@
-package org.cashempire.gamelaunch.util;
-
-public class Static {
-    public final static String HOMEPATH = "v1/home/";
-    public final static String GAMELAUNCHPATH = "gamelaunch";
-    public static final String GAMESPATH = "games";
-    
-}
